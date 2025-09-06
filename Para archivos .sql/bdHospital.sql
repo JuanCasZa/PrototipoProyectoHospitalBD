aaa9c8d0c327d4dff24e7da25330729a6b78d6bf
JuanCasZa <juanjose-castro2@hotmail.com>
2025-09-06 13:16:57 -0500

    Codigo de tablas, mas no creadas aún

--- Para archivos .sql/bdHospital.sql
+++ Para archivos .sql/bdHospital.sql
@@ -13,3 +13,42 @@ create table Departamentos
 	[JefeDepartamento] int references [JefeDepartamentos]([Id]) not null,
 );
 
+create table Medicos
+(
+	[Id] int primary key identity(1,1),
+	[Nombre] nvarchar(30) not null,
+	[Apellido] nvarchar(40) not null,
+	[Especialidad] nvarchar(40) not null,
+	[Departamento] int references [Departamentos]([Id]) not null
+);
+
+create table Pacientes
+(
+	[Id] int primary key identity(1,1),
+	[Nombre] nvarchar(30) not null,
+	[Apellido] nvarchar(40) not null,
+	[Direccion] nvarchar(200) not null
+);
+
+create table HistoriasClinicas
+(
+	[Id] int primary key identity(1,1),
+	[Fecha] smalldatetime default getdate() not null,
+	[Diagnostico] nvarchar(200) not null,
+	[Tratamiento] nvarchar(200) not null,
+	[Paciente] int references [Pacientes]([Id]) not null
+);
+
+create table Atienden
+(
+	[Id] int primary key identity(1,1),
+	[Medico] int references [Medicos]([Id]) not null,
+	[Paciente] int references [Pacientes]([Id]) not null
+);
+
+create table Citas
+(
+	[Id] int primary key identity(1,1),
+	[Fecha] smalldatetime default getdate(),
+	[Especialidad] varchar
+);
